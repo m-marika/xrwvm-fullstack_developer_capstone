5b5d040be0788ab683fdeb37d8d5497f62624694
m-marika1 <m-marika1@yandex>
2024-05-30 03:07:07 -0400

    some changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -125,9 +125,12 @@ def add_review(request):
 
 
 def get_cars(request):
-    count = CarMake.objects.count()
-    if count == 0:
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
-    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
-    return JsonResponse({"CarModels": cars})
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
